ffbee02b2cf7b51686e204a4bdca9a484814a84f
Vlad Hociotă <vlad.hociota@geminisols.com>
2017-07-12 16:33:13 +0300

    add Media

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "S3AppModel",
     dependencies: [
         .Package(url: "https://github.com/GeminiSolutions/Auth.git", majorVersion: 0),
+        .Package(url: "https://github.com/GeminiSolutions/Media.git", majorVersion: 0),
         .Package(url: "https://github.com/GeminiSolutions/Places.git", majorVersion: 0),
         .Package(url: "https://github.com/GeminiSolutions/Users.git", majorVersion: 0),
     ]
